38f0be740f4ef94c8de92e9ae4f893888c235155
NullerGoej <bastian2010@live.dk>
2024-06-07 15:49:24 +0200

    Updated some code

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -14,7 +14,7 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     fetchUserSuccess: (state, action) => {
-      const { firstname, lastname, email, image, phone} = action.payload.user;
+      const { firstname, lastname, email, image, phone } = action.payload.user;
       return {
         ...state,
         firstname,
@@ -34,14 +34,22 @@ export const fetchUserData = (navigate, location) => {
     axios.get('https://zealand.moedekjaer.dk/final/api/public/api/user', {
       headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
     }).then((response) => {
-      dispatch(fetchUserSuccess(response.data));
-      if (response.data.user.phone === 0) {
-        navigate('/setup-1');
-      } else if (response.data.user.phone === 1) {
-        navigate('/setup-2');
-      } else if (location === '/setup-1' || location === '/setup-2') {
-        navigate('/');
+      if (!response.data.user) {
+        if (location != '/login' && location != '/register') {
+          localStorage.removeItem('token');
+          navigate('/login');
+        }
+      } else {
+        dispatch(fetchUserSuccess(response.data));
+        if (response.data.user.phone === 0) {
+          navigate('/setup-1');
+        } else if (response.data.user.phone === 1) {
+          navigate('/setup-2');
+        } else if (location === '/setup-1' || location === '/setup-2') {
+          navigate('/');
+        }
       }
+
     }).catch(() => {
       localStorage.removeItem('token');
       navigate('/login');
